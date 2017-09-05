4766b0d3d2fae2512a283c405c7bbb9ecd9464ee
John Ryan <johnryan432@gmail.com>
2017-09-04 20:42:20 -0500

    Post is uploaded to Firebase

--- InstagramFirebase/AddPhoto/SharePhotoController.swift
+++ InstagramFirebase/AddPhoto/SharePhotoController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SharePhotoController: UIViewController {
     
@@ -43,7 +44,6 @@ class SharePhotoController: UIViewController {
         let containerView = UIView()
         containerView.backgroundColor = .white
         
-        
         view.addSubview(containerView)
         containerView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 100)
         
@@ -52,15 +52,62 @@ class SharePhotoController: UIViewController {
         
         containerView.addSubview(textView)
         textView.anchor(top: containerView.topAnchor, left: imageView.rightAnchor, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 4, paddingBottom: 0, paddingRight: 3, width: 0, height: 0)
-        
     }
     
     @objc func handleShare() {
-        print("Sharing Photo")
+        guard let caption = textView.text, caption.characters.count > 0 else { return }
+        guard let image = selectedImage else { return }
+        guard let uploadData = UIImageJPEGRepresentation(image, 0.5) else { return }
+        
+        navigationItem.rightBarButtonItem?.isEnabled = false
+        
+        let filename = NSUUID().uuidString
+        Storage.storage().reference().child("posts").child(filename).putData(uploadData, metadata: nil) { (metadata, err) in
+            if let err = err {
+                self.navigationItem.rightBarButtonItem?.isEnabled = false
+                print("Failed to upload post image", err)
+            }
+            guard let imageUrl = metadata?.downloadURL()?.absoluteString else { return }
+            
+            print("Successfully Uploaded image to", imageUrl)
+            self.saveToDatabaseWithImageUrl(imageUrl: imageUrl)
+        }
     }
     
+    fileprivate func saveToDatabaseWithImageUrl(imageUrl: String) {
+        guard let caption = textView.text else { return }
+        
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        guard let postImage = selectedImage else { return }
+        
+        let userPostRef = Database.database().reference().child("posts").child(uid)
+        let ref = userPostRef.childByAutoId()
+        
+        let values = ["imageUrl": imageUrl, "caption": caption, "imageWidth": postImage.size.width, "imageHeight": postImage.size.height, "creationDate": Date().timeIntervalSince1970] as [String : Any]
+        
+        ref.updateChildValues(values) { (err, ref) in
+            if let err = err {
+                self.navigationItem.rightBarButtonItem?.isEnabled = true
+                print("Failed to save post to DB", err)
+                return
+            }
+            print("Successfully saved post to DB")
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
     override var prefersStatusBarHidden: Bool {
         return true
     }
-    
 }
+
+
+
+
+
+
+
+
+
+
+
