552216e4a193218a927be7b53079ee7838f184f7
John Ryan <johnryan432@gmail.com>
2017-09-10 16:24:24 -0500

    Implemented search controller beginnings

---
+++ InstagramFirebase/Search/UserSearchController.swift
@@ -0,0 +1,69 @@
+//
+//  UserSearchController.swift
+//  InstagramFirebase
+//
+//  Created by John Ryan on 9/10/17.
+//  Copyright © 2017 John Ryan. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.placeholder = "Enter username"
+        sb.barTintColor = .gray
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor.rgb(red: 230, green: 230, blue: 230)
+        return sb
+    }()
+    
+    let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .white
+        
+        navigationController?.navigationBar.addSubview(searchBar)
+        
+        let navBar = navigationController?.navigationBar
+        
+        searchBar.anchor(top: navBar?.topAnchor, left: navBar?.leftAnchor, bottom: navBar?.bottomAnchor, right: navBar?.rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
+        
+        collectionView?.register(UserSearchCell.self, forCellWithReuseIdentifier: cellId)
+        
+        collectionView?.alwaysBounceVertical = true
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 66)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
