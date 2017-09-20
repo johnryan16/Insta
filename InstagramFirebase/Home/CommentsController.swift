5a7f09880596e6f85cc097be6675576010537b52
John Ryan <johnryan432@gmail.com>
2017-09-19 19:20:24 -0500

    Implemented auto sizing of comment cells

--- InstagramFirebase/Home/CommentsController.swift
+++ InstagramFirebase/Home/CommentsController.swift
@@ -19,7 +19,11 @@ class CommentsController: UICollectionViewController, UICollectionViewDelegateFl
         super.viewDidLoad()
         
         navigationItem.title = "Comments"
-        collectionView?.backgroundColor = .red
+        collectionView?.backgroundColor = .white
+        
+        collectionView?.alwaysBounceVertical = true
+        collectionView?.keyboardDismissMode = .interactive
+        
         
         /* ----______--------_________------??Maybe causes problem later??-----_______-----_____------____
         collectionView?.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: -50, right: 0)
@@ -44,11 +48,18 @@ class CommentsController: UICollectionViewController, UICollectionViewDelegateFl
             
             guard let dictionary = snapshot.value as? [String: Any] else { return }
             
-            let comment = Comment(dictionary: dictionary)
-//            print(comment.text, comment.uid)
+            guard let uid = dictionary["uid"] as? String else { return }
             
-            self.comments.append(comment)
-            self.collectionView?.reloadData()
+            Database.fetchUserWithUID(uid: uid, completion: { (user) in
+                
+                let comment = Comment(user: user, dictionary: dictionary)
+                
+                self.comments.append(comment)
+                self.collectionView?.reloadData()
+                
+            })
+            
+            
             
             
         }) { (err) in
@@ -61,7 +72,22 @@ class CommentsController: UICollectionViewController, UICollectionViewDelegateFl
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 50)
+        
+        let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
+        let dummyCell = CommentCell(frame: frame)
+        dummyCell.comment = comments[indexPath.item]
+        dummyCell.layoutIfNeeded()
+        
+        let targetSize = CGSize(width: view.frame.width, height: 50)
+        let estimatedSize = dummyCell.systemLayoutSizeFitting(targetSize)
+        
+        let height = max(40 + 8 + 8, estimatedSize.height)
+        
+        return CGSize(width: view.frame.width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -97,6 +123,11 @@ class CommentsController: UICollectionViewController, UICollectionViewDelegateFl
         
         containerView.addSubview(commentTextField)
         self.commentTextField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitButton.leftAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        let lineSeparatorView = UIView()
+        lineSeparatorView.backgroundColor = UIColor.rgb(red: 230, green: 230, blue: 230)
+        containerView.addSubview(lineSeparatorView)
+        lineSeparatorView.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: nil, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 1)
         
         return containerView
     }()
