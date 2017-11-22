2c29380479e6bb172e65dd53d2025efd1d0d2b2a
John Ryan <johnryan432@gmail.com>
2017-11-22 10:56:06 -0600

    removed remote notifications temporarily

--- InstagramFirebase/AppDelegate.swift
+++ InstagramFirebase/AppDelegate.swift
@@ -25,7 +25,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         
 //        if #available(iOS 10.0, *) {
-//            //This fucker is so slow and lame/Notifications activate
 //            UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
 //            
 //            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
@@ -36,30 +35,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //            //todo: Implement window
 //        }
 //        application.registerForRemoteNotifications()
-//        
+        
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        //TouchID window, Multitask after Doubletap
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        //After selecting another app in multitasking window OR going home
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
+        //After selecting from multitak window or from home screen
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        // Happens on ANY Load
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        //Manual Close
     }
 
 
